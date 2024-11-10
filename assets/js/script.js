b1f2aaff786ceca1fd75e8ba2d37491a0588e795
Roeich <roeichsheikh@gmail.com>
2024-11-10 16:14:45 +0600

    added faq section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,35 @@ $(document).ready(function() {
     $(".menu_toggleBtn,.header_backDrop").click(function() {
         $(".header").toggleClass("active_header");
     });
+
+    // ... feedback slider
+    $(".feedback_slider").owlCarousel({
+        items: 1,
+        loop: false,
+        nav: false,
+        dots: false,
+        onInitialized: updateCounter,
+        onChanged: updateCounter
+    });
+
+    function updateCounter(event) {
+        var totalItems = event.item.count;
+        var currentItem = event.item.index - event.relatedTarget._clones.length / 2 + 1;
+
+        if (currentItem > totalItems) {
+            currentItem = currentItem - totalItems;
+        }
+
+        $(".current_number").text(currentItem.toString().padStart(2, '0'));
+        $(".total_number").text(`/ ${totalItems.toString().padStart(2, '0')}`);
+    }
+
+    $(".feedback_sliderBox .btn_next").click(function() {
+        $(".feedback_slider").trigger("next.owl.carousel");
+    });
+
+    $(".feedback_sliderBox .btn_prev").click(function() {
+        $(".feedback_slider").trigger("prev.owl.carousel");
+    });
     /* ___________________ end home page ___________________ */
 });
