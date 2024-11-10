e37e33d91c060526d10a815a3452f00257110d3f
Roeich <roeichsheikh@gmail.com>
2024-11-10 22:57:53 +0600

    completed desktop version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
     /* ___________________ start home page ___________________ */
+    
     // ... header secction
     $(".menu_toggleBtn,.header_backDrop").click(function() {
         $(".header").toggleClass("active_header");
     });
+
+    function checkScroll() {
+        if ($(window).scrollTop() > 50) {
+            $("body").addClass('scrolled_header');
+        } else {
+            $("body").removeClass('scrolled_header');
+        }
+    }
+    checkScroll();
+    $(window).on('scroll', function() {
+        checkScroll();
+    });
+    
 
     // ... feedback slider
     $(".feedback_slider").owlCarousel({
