4e594781e8aef5daac61a488a766e194e12775b9
Roeich <roeichsheikh@gmail.com>
2024-11-10 14:50:26 +0600

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    /* ___________________ start home page ___________________ */
+    // ... header secction
+    $(".menu_toggleBtn,.header_backDrop").click(function() {
+        $(".header").toggleClass("active_header");
+    });
+    /* ___________________ end home page ___________________ */
+});
